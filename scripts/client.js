e71213e5ac85e748ccde3d83b691fbf3ffe28409
Bradley Hennen <bradhennen5@gmail.com>
2019-03-11 10:59:58 -0500

    Fixed form funcitons with event handler. Cleaned up magic numbers

--- scripts/client.js
+++ scripts/client.js
@@ -11,17 +11,21 @@ class Employee {
 }
 
 const roster = [];
+const NUMBER_OF_MONTHS = 12;
+const MONTHLY_SALARY_LIMIT = 20000;
 
 $(document).ready(readyNow);
 
 
 function readyNow() {
-  $('#submitButton').on('click', addEmployee);
-  // $('#employeeList').on('click', '.taco', '.clearButton', removeEmployee);
+  $('#submitButton').on('click', handleSubmit);
   $('#tableBody').on('click', 'button.clearButton', removeEmployee);
 }
 
-
+function handleSubmit(event) {
+  event.preventDefault();
+  addEmployee();
+}
 
 //Adds employee information to the array and table as well as checks inputs
 function addEmployee() {
@@ -64,14 +68,14 @@ function ifMonthlyExceeds() {
   for (let i = 0; i < roster.length; i++) {
     totalMonthly = totalMonthly + Number(roster[i].salary);
   }
-  totalMonthly = totalMonthly.toFixed(0) / 12;
+  totalMonthly = totalMonthly.toFixed(0) / NUMBER_OF_MONTHS;
 
-  if (totalMonthly > 20000) {
+  if (totalMonthly > MONTHLY_SALARY_LIMIT) {
     return $('#monthlySum').css({"background-color": "rgba(255, 0, 0, 0.5)"}).append(`<span id="total">$ ${addCommasToNumbers(totalMonthly.toFixed(2))}</span>`);
   } else {
     return $('#monthlySum').css('background-color', 'transparent').append(`<span id="total">$ ${addCommasToNumbers(totalMonthly.toFixed(2))}</span>`);
   }
-}
+}//ifMonthlyExceeds End
 
 //Adds employee information to the DOM via table 
 function render() {
@@ -94,11 +98,11 @@ function render() {
 //Removes employee from DOM table and array if user hits the clear button
 function removeEmployee() {
   let employeeData = $(this).closest("tr").data();
-  console.log('On Click Employee Data:', employeeData);
+  //console.log('On Click Employee Data:', employeeData);
   
   for (let i = 0; i < roster.length; i++) {
     let employee = roster[i];
-    console.log('Roster:', employee);
+    //console.log('Roster:', employee);
     
     
     if (employee.firstName === employeeData.firstName && 
@@ -107,10 +111,9 @@ function removeEmployee() {
       employee.title === employeeData.title && 
       employee.salary === employeeData.salary) {
 
-        console.log('Employee Removed');
+        console.log('Employee Removed: ', employee.firstName, employee.lastName);
         roster.splice(i, 1);
         ifMonthlyExceeds();
-        
           
     } 
   }
@@ -120,4 +123,4 @@ function removeEmployee() {
 //Adds commas to numbers
 function addCommasToNumbers(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+} //addCommasToNumbers End (Found on StackOverflow)
